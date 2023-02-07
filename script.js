5cc998e3aa2fcda81778be902818cb04806fa75b
LeoonWi <redwind090@gmail.com>
2023-02-08 00:34:35 +0300

    Математические операции

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let a = 1 + 2 + 3;
-alert(a);
+let a = 10, b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+
+let c = 10, d = 5;
+let result = c + d;
+alert(result);
+
+a = 1, b = 2, c = 3;
+alert(a + b + c);
+
+a = 10, b = 5;
+c = a + b, d = 7, result = c + d;
+alert(result);
