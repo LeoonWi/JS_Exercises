c9ab52e3ea06b905b31beab3c6471a40e130d34b
LeoonWi <redwind090@gmail.com>
2023-02-08 00:43:14 +0300

    Дроби

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let a = 10, b = 2;
+let a = 1.5, b = 0.75;
 alert(a + b);
-alert(a - b);
-alert(a * b);
-alert(a / b);
-
-let c = 10, d = 5;
-let result = c + d;
-alert(result);
-
-a = 1, b = 2, c = 3;
-alert(a + b + c);
-
-a = 10, b = 5;
-c = a + b, d = 7, result = c + d;
-alert(result);
