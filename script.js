bfcaead9df832b0fa04da9c5f0395bb291941475
LeoonWi <redwind090@gmail.com>
2023-02-08 17:43:57 +0300

    Строки с цифрами

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let a = 'abcde';
-alert(a[a.length - 1]);
-alert(a[a.length - 2]);
-alert(a[a.length - 3]);
+let a = '12345';
+let b = 0;
+for(let i = 0; i < a.length; i++) {
+    b = b + Number(a[i]);
+}
+alert(b);
+
+a = String(12345);
+b = 0;
+for(let i = 0; i < a.length; i++) {
+    b = b + Number(a[i]);
+}
+alert(b);
+
+b = 1;
+for(let i = 0; i < a.length; i++) {
+    b = b * a[i];
+}
+alert(b);
+
+b = a[4] + a[3] + a[2] + a[1] + a[0];
+alert(b);
