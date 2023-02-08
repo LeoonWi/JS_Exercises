95fde098c9752f64e903fa42abbf7e57da4b7cf2
LeoonWi <redwind090@gmail.com>
2023-02-08 16:36:28 +0300

    Константы

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let a = 1, b = '1', c = true, d;
-console.log(a), console.log(b), console.log(c), console.log(d);
-alert(e);
+let r = 15;
+const PI = 3.14;
+let P = 2 * PI * r;
+alert(P);
