94e3a0014208bdc467053c3ebd792ccb316b547e
LeoonWi <redwind090@gmail.com>
2023-02-08 16:51:57 +0300

    Преобразование к числу

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let r = 15;
-const PI = 3.14;
-let P = 2 * PI * r;
-alert(P);
+let a = '10', b = '20';
+alert(Number(a) + Number(b));
+
+//alert( Number('2') + Number('3')); Выведет 5
+
+//alert(2 + Number('3')); Выведет 5
+
+//alert('2' + Number('3')); Выведет 23
+
+a = '2', b = '3';
+alert(+a + +b)
