3300156d72adba4fb6fe0fdb908e8c0331abdd61
LeoonWi <redwind090@gmail.com>
2023-02-08 16:13:49 +0300

    Шаблонные строки и перенос на след. строку

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let str = 'My name is LeoomWi!';
-alert(str.length);
+let str1 = 'xxx';
+let str2 = 'yyy';
+let txt  = `aaa ${str1} bbb ${str2} ccc`;
+alert(txt);
+
+txt = `a \nb \nc`
+alert(txt);
+
+txt = `1
+2
+3`;
+alert(txt);
+//2 способа переноса строк
