5ecfb866b6b509ce9bce82b19fe1092f39ff8d2b
LeoonWi <redwind090@gmail.com>
2023-02-08 16:29:41 +0300

    Работа с консолью

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let a;
-alert(a);
-
-a = null;
-alert(a);
-
-let b;
-a = true, b = false;
-alert(a);
-alert(b);
-a = 'abc', b = 'def';
-alert(a * b);
-alert(10/0);
-alert(-10/0);
+let a = 1, b = '1', c = true, d;
+console.log(a), console.log(b), console.log(c), console.log(d);
+alert(e);
