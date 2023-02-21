594becc9b7ce6b615eb03b0e0f1be18cd1e18e83
LeoonWi <redwind090@gmail.com>
2023-02-21 22:38:58 +0300

    Функция prompt

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let a = 0.1 * 0.2;
-let b = 0.3 - 0.1;
+let name = prompt('Введите ваш возраст:');
+alert("Возраст: " + name);
 
-alert(a.toFixed(2));
-alert(b.toFixed(2));
+let num1 = prompt('Введите первое число:');
+let num2 = prompt('Введите второе число:');
+alert(Number(num1) + Number(num2));
+
+num1 = prompt('Введите одну сторону квадрата:');
+alert(Number(num1)**2);
+
+num1 = prompt('Введите первую сторону прямоугольника:');
+num2 = prompt('Введите вторую сторону прямоугольника:');
+alert((Number(num1) + Number(num2)) * 2);
