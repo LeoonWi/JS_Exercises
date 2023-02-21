57e92ee57b77737d1bf88f7124e86bead1291a42
LeoonWi <redwind090@gmail.com>
2023-02-21 22:39:48 +0300

    Неточные вычисления

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let name = prompt('Введите ваш возраст:');
-alert("Возраст: " + name);
+let a = 0.1 * 0.2;
+let b = 0.3 - 0.1;
 
-let num1 = prompt('Введите первое число:');
-let num2 = prompt('Введите второе число:');
-alert(Number(num1) + Number(num2));
-
-num1 = prompt('Введите одну сторону квадрата:');
-alert(Number(num1)**2);
-
-num1 = prompt('Введите первую сторону прямоугольника:');
-num2 = prompt('Введите вторую сторону прямоугольника:');
-alert((Number(num1) + Number(num2)) * 2);
+alert(a.toFixed(2));
+alert(b.toFixed(2));
