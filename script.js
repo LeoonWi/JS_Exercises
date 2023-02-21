77bf27be44ea6f8eb9cec5d5eebb4420e721e694
LeoonWi <redwind090@gmail.com>
2023-02-21 22:15:03 +0300

    Операции инкремента и декремента

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let text = '12345';
-let res = 0;
-for(let i = 0; i < text.length; i++) {
-    res = res + Number(text[i]);
-}
-alert(res);
-
-let num = 12345;
-num = String(num);
-res = 0
-for(let i = 0; i < num.length; i++) {
-    res = res + Number(num[i]);
-}
-alert(res);
-
-res = 1
-for(let i = 0; i < num.length; i++) {
-    res = res * num[i];
-}
-alert(res);
-
-let new_num = '';
-for(let i = 4; i >= 0; i--) {
-    new_num += num[i];
-}
-alert(new_num);
+let num = 10;
+num++;
+num++;
+num--;
+alert(num);
