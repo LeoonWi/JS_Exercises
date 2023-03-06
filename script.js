12c7705cc25bd195816031d710118d2a785b8a59
LeoonWi <redwind090@gmail.com>
2023-03-06 20:38:23 +0300

    Поиск ошибок в коде с массивами

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let arr = ['a', 'b', 'c'];
-document.write(arr + '<br>');
-delete arr[2];
-document.write(arr);
