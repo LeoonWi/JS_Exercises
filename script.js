e8a0743083d84b905d5a90dad5d0e253566abbf7
LeoonWi <redwind090@gmail.com>
2023-03-06 18:45:34 +0300

    Практика на операции

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
+
+document.write(24 * 60 * 60 + '<br>');
+document.write((24 * 30) * 60 * 60 + '<br>');
+document.write((24 * 365) * 60 * 60 + '<br>');
+document.write(24 * 60 + '<br>');
+document.write((24 * 365) * 60 + '<br>');
+document.write(1024 * 1024 + '<br>');
+document.write(1024 * 1024 * 1024 + '<br>');
+document.write(1024 * 1024 * 1024 + '<br>');
