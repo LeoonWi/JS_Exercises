1453c084d45ca8d7d76dbf3ffa1862bb26dbbcb6
LeoonWi <redwind090@gmail.com>
2023-03-06 20:30:41 +0300

    Разреженные массивы

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
 let arr = [];
-arr[0] = 'a';
-arr[1] = 'b';
-arr[2] = 'c';
-document.write(arr + '<br>');
-
-arr = [1, 2, 3]
 arr[3] = '4';
-arr[4] = '5';
-document.write(arr);
+arr[8] = '9';
+document.write('Длина массива: ' + arr.length)
