c7ea68c0fff0751379ee51eddfece83649ff667f
LeoonWi <redwind090@gmail.com>
2023-03-06 20:35:52 +0300

    Удаление элементов в массиве

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
 let arr = ['a', 'b', 'c'];
-let key = 2;
-document.write(arr[key] + '<br>');
-arr = [1, 2, 3, 4, 5];
-let key1 = 1;
-let key2 = 2;
-document.write(arr[key1] + arr[key2]);
+document.write(arr + '<br>');
+delete arr[2];
+document.write(arr);
