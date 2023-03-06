4f35b9536aa0c7f059ee97ac0556df05b8a04e90
LeoonWi <redwind090@gmail.com>
2023-03-06 18:38:27 +0300

    Поиск ошибок в коде

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
-
-document.write('<i>Какой-то текст</i><br>');
-let str = 'text';
-document.write('<i>' + str + '</i>')
-document.write('<br>1<br>2<br>3<br>4<br>5')
