f01304cd4dace5bbf5b79b5ff052733543ced429
LeoonWi <redwind090@gmail.com>
2023-03-06 18:12:55 +0300

    Вывод текст в документ

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let name = prompt('Введите ваш возраст:');
-alert("Возраст: " + name);
-
-let num1 = prompt('Введите первое число:');
-let num2 = prompt('Введите второе число:');
-alert(Number(num1) + Number(num2));
-
-num1 = prompt('Введите одну сторону квадрата:');
-alert(Number(num1)**2);
-
-num1 = prompt('Введите первую сторону прямоугольника:');
-num2 = prompt('Введите вторую сторону прямоугольника:');
-alert((Number(num1) + Number(num2)) * 2);
+document.write('<i>Какой-то текст</i><br>');
+let str = 'text';
+document.write('<i>' + str + '</i>')
+document.write('<br>1<br>2<br>3<br>4<br>5')
