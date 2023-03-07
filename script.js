229683d730ff8dd6fe1975e445dd5f1f4de871d9
LeoonWi <redwind090@gmail.com>
2023-03-07 19:49:14 +0300

    Массив ключей объекта

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let obj = {};
-obj[1] = '1';
-obj[2] = '2';
-obj[3] = '3';
-document.write('Стало: ' + obj[1] + ' ' + obj[2] + ' ' + obj[3]);
+let obj = {x: 1, y: 2, z: 3};
+let keys = Object.keys(obj);
+document.write(keys);
