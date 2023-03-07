4c0acd767794998b7d26abf7c04b6fd553ba74a1
LeoonWi <redwind090@gmail.com>
2023-03-07 19:43:23 +0300

    Добавление нового элемента

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let obj = {x: 1, y: 2, z: 3};
-document.write('Было: ' + obj.x + ' ' + obj.y + ' ' + obj.z + '<br>');
-obj.x = obj.x**2;
-obj['y'] = obj.y**2;
-obj.z = obj.z**2;
+let obj = {};
+obj.x = 1;
+obj['y'] = 2;
+obj.z = 3;
 document.write('Стало: ' + obj.x + ' ' + obj.y + ' ' + obj.z);
