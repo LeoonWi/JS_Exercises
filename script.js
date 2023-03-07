ce88e958454675b5ef20794fd19db50fd2e5a6cc
LeoonWi <redwind090@gmail.com>
2023-03-07 19:52:36 +0300

    Ключи объектов из переменных

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
 let obj = {x: 1, y: 2, z: 3};
-document.write(Object.keys(obj).length);
+let key = 'y';
+document.write(obj[key]);
