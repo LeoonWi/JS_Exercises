9f7daf949e33bdcb0083a90fa9d8916da5728173
LeoonWi <redwind090@gmail.com>
2023-03-07 19:34:51 +0300

    Ограничения на ключи объектов

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let date = {year: 2023, month: 'april', day: '7'};
-document.write(date.year + ' ' + date.month +  ' ' + date.day);
+let obj = {
+	'1a': 1,
+	b2: 2,
+	'с-с': 3,
+	'd 4': 4,
+	e5: 5
+};
+
+let obj2 = {
+	'1a': 1,
+	b2: 2,
+	'с-с': 3,
+	'd 4': 4,
+	e5: 5
+};
+
+document.write(obj2['1a']);
+document.write(obj2.b2);
+document.write(obj2['с-с']);
+document.write(obj2['d 4']);
+document.write(obj2.e5);
