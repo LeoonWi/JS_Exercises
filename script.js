bd928608dba00ddc31e5a511ee0f974e18bd0b19
LeoonWi <redwind090@gmail.com>
2023-03-07 19:29:06 +0300

    Свойства объектов

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-let user = {name: 'Виталий', surname: 'Шагин', patronymic: 'Александрович'};
-document.write(user['name'] + ' ' + user['surname'] + ' ' + user['patronymic']);
+let date = {year: 2023, month: 'april', day: '7'};
+document.write(date.year + ' ' + date.month +  ' ' + date.day);
