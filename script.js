12b37c215213da191ab7d818c53784101fd1e587
LeoonWi <redwind090@gmail.com>
2023-03-07 19:50:34 +0300

    Длина объекта с помощью массива ключей

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
 let obj = {x: 1, y: 2, z: 3};
-let keys = Object.keys(obj);
-document.write(keys);
+document.write(Object.keys(obj).length);
