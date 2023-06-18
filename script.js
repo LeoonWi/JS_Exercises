cc7070bf969eb2631d27467afc74da26657a1fa3
LeoonWi <redwind090@gmail.com>
2023-06-19 00:18:23 +0300

    Проверка символов строки

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
+
+// let string = 'dsdqwdqwd';
+
+// if (string[0] == 'D' || string[0] == 'd') {
+//    console.log('da');
+// } else {
+//    console.log('net');
+// }
+
+// let string = '2dfasdfa43afe3';
+
+// if (string[string.length - 1] == '3' || string[string.length - 1] == '3') {
+//    console.log('da');
+// } else {
+//    console.log('net');
+// }
+
+let string = 'tfsdfaw4fa4';
+
+if (string[0] == 't' || string[0] == 'f') {
+   console.log('da');
+} else {
+   console.log('net');
+}
