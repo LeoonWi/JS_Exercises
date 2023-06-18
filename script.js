b6cf073416c6d3be9b4a6987610f79262ab108da
LeoonWi <redwind090@gmail.com>
2023-06-19 00:21:10 +0300

    Проверка цифр на четность

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
 "use strict"; //Это включение строго режима, чтобы браузер включал все современные возможности языка.
 
-// let string = 'dsdqwdqwd';
+let num = 4;
 
-// if (string[0] == 'D' || string[0] == 'd') {
-//    console.log('da');
-// } else {
-//    console.log('net');
-// }
-
-// let string = '2dfasdfa43afe3';
-
-// if (string[string.length - 1] == '3' || string[string.length - 1] == '3') {
-//    console.log('da');
-// } else {
-//    console.log('net');
-// }
-
-let string = 'tfsdfaw4fa4';
-
-if (string[0] == 't' || string[0] == 'f') {
-   console.log('da');
+if (num % 2 == 0) {
+   console.log('da')
 } else {
-   console.log('net');
+   console.log('net')
 }
